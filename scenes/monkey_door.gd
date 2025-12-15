819480cb78a6523e507857dcf97522c208cc542c
Kurooco <128552915+Kurooco@users.noreply.github.com>
2025-12-14 19:50:09 -0500

    created monkey door

---
+++ scenes/monkey_door.gd
@@ -0,0 +1,24 @@
+extends PathFollow3D
+
+@export var interaction_area : InteractionArea
+@export var speed : float = 1.0
+@export var hide_at_end = false
+var running = false
+var p = 0.0
+
+func _ready():
+	$NumberDoor.texture = load("res://art/2d/door.png")
+	interaction_area.automatic = true
+	interaction_area.show_prompt = false
+	interaction_area.one_shot = true
+	interaction_area.activated.connect(run)
+
+func run():
+	running = true
+	$NumberDoor.texture = load("res://art/2d/monkey_door.png")
+	
+func _process(delta):
+	if(running):
+		progress_ratio += delta*speed
+		if(progress_ratio >= 1 && hide_at_end):
+			hide()
